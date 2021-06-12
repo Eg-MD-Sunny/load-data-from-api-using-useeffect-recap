642a39016668be7470b996c4c82d35001c5773d1
Naim Zaman <egnaimur@gmail.com>
2021-06-12 21:32:18 +0600

    Load Data from API Using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+	const [nayoks,setNayoks] = useState([])
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(res => res.json())
+		.then(data => setNayoks(data))
+	},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<header className="App-header">
+			{
+				nayoks.map(item => <Nayoks name={item.name} uniqueKey={item.id}></Nayoks>)
+			}
+		</header>
+	</div>
   );
 }
-
+function Nayoks(props){
+	// console.log(props);
+	const nayoksStyle = {
+		border:'2px solid white',
+		margin:'20px',
+		borderRadius: '7px'
+	}
+	return (
+		<div style={nayoksStyle}>
+			<h4>Your id - <span style={{color:'cyan'}}>{props.uniqueKey}</span></h4>
+			<h1>Ami Nayok - <span style={{color:'yellow'}}>{props.name || 'Naim'}</span></h1>
+			<h3>I have done 5 movies in {props.age || 20} years</h3>
+		</div>
+	)	
+}
 export default App;
